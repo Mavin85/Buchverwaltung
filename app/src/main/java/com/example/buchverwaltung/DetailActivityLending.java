89ee906680d091707a28585026c46afc2e4932cf
emanuelfuchs <emanuel-fuchs@t-online.de>
2020-12-01 14:09:55 +0100

    added activity_detail_book_adding.xml added activity_detail_lending.xml

---
+++ app/src/main/java/com/example/buchverwaltung/DetailActivityLending.java
@@ -0,0 +1,14 @@
+package com.example.buchverwaltung;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailActivityLending extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_lending);
+    }
+}
